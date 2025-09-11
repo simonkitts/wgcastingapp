34c3b30de842b325ce141fa484bf4c0759ef9f3f
sktn82 <simon.kittsteiner@mni.thm.de>
2025-09-12 01:34:41 +0200

    added much stuff: backup service, kommentsection, more readability in the calendar, appointment update fix, ...

--- server/driveindex.js
+++
@@ -1,4 +0,0 @@
-// Deprecated: This quickstart script is no longer used.
-// Google Drive is handled by server/backupService.js with OAuth via env vars.
-// This file is intentionally left blank to avoid confusion.
-
